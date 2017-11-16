bc331e355b2283009da41f7414d47c40679b342b
Adam <a.stasiun@icloud.com>
2017-11-16 16:56:59 +0100

    js full

--- javascript/main.js
+++ javascript/main.js
@@ -24,7 +24,7 @@ function wypiszWKonsoli() {
     console.log('123');
 }
 
-var nowaFunkcja = function() {
+var nowaFunkcja = function () {
     console.log('456');
 }
 
@@ -70,8 +70,10 @@ console.log(tablica[3]);
 tablica.push('Michał');
 console.log(tablica[4]);
 
-tablica[1] ='podmieniony';
-console.log(tablica);
+//tablica[1] ='podmieniony';
+//console.log(tablica);
+
+
 //pop - ZAWSZE usuwa ostatni element tablicy
 tablica.pop();
 console.log(tablica);
@@ -83,3 +85,244 @@ console.log(tablica);
 //shift() - usuwa pierwszy element tablicy
 tablica.shift();
 console.log(tablica);
+
+//length - zwraca dlugosc tablicy
+console.log(tablica.length);
+
+//join wypisuje elementy tablicy jako string - po znaku 
+console.log(tablica.join());
+
+//reverse() - odwraca kolejnosc elementow tablicy
+console.log(tablica.reverse());
+
+//sort sotruje dane wedle wzoru - najpierw znaki specjalne, pozniej cyfry/liczby, duze litery, male litery
+console.log(tablica.sort());
+
+var string = 'Ala ma kota 105';
+
+//split() - dzieli string po podanym parametrze i wrzuca je do tablicy
+var res = string.split('')
+console.log(res);
+console.log(res.join(''));
+console.log(res.sort().join());
+
+var osoba = {
+    name: 'Marcin',
+    height: '180',
+    print: function () {
+        console.log(this.name);
+    }
+}
+
+console.log(osoba.name + ' ' + osoba.height);
+osoba.print();
+
+
+console.log(osoba);
+
+osoba.weight = 70;
+console.log(osoba);
+
+osoba.printDetail = function () {
+    console.log(this.name + ' ' + this.height + ' ' + this.weight);
+}
+console.log(osoba);
+
+osoba.printDetail();
+
+//klasy obiektu - szablon dla obiektu
+class Osoba {
+    constructor(imie, nazwisko, height) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.height = height;
+    }
+
+    wyswietlInfo() {
+        console.log('Imię: ' + this.imie + ' ' + 'Nazwisko: ' + this.nazwisko + ' ' + 'Wzrost: ' + this.height);
+    }
+}
+
+var krystian = new Osoba('Krystian', 'Dziopa', 184);
+
+var marcin = new Osoba('Marcin', 'G', 190);
+
+krystian.wyswietlInfo();
+marcin.wyswietlInfo();
+
+//obiekt wbudowany math
+console.log('PI= ' + Math.PI);
+
+//losowanie liczb od 0 - 1
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
+
+//pow(liczba podnoszona, potega)
+console.log(Math.pow(10, 2));
+//modulo zwraca resztę z dzielenia
+console.log(3 % 2);
+//+=
+var a = 10;
+a = a + 10;
+console.log(a);
+
+
+//operatory porownania
+var string1 = '10';
+var number1 = 10;
+
+console.log(string1 == number1);
+
+//operator warunkowy
+//(warunek) ? jezeli_prawda : jezeli_nieprawda;
+var ujemna = -10;
+var wynik = (ujemna > 0) ? 'jest wieksza od 0' : 'nie jest wieksza od 0';
+console.log(wynik);
+
+//if najprostszej postaci wykonuje sie wtedy i tylko wtedy, gdy podany warunek jest spelniony (to znaczy ma wartosc true)  
+if (ujemna < 0) {
+    console.log('ujemna jest mniejsza od 0');
+}
+
+//w bloku else instrukcje do wykonania, gdy warunek niespelniony (to znaczy ma wartosc false)
+if (ujemna > 0) {
+    console.log('ujemna jest mniejsza od 0');
+} else {
+    console.log('warunek nie zostal spelniony');
+}
+
+var dodatnia = 100;
+//else if sprawdza, czy kolejny (enty) podany warunek jest prawdziwy
+if (dodatnia < 0) {
+    console.log('dodatnia jest mniejsza od 0');
+} else if (dodatnia == 100) {
+    console.log('podales liczbe 100');
+} else {
+    console.log('warunek nie zostal spelniony');
+}
+
+//instrukcja switch
+var kolor = 'zielony';
+
+switch (kolor) {
+    case 'zielony':
+        console.log('podales kolor zielony');
+        break;
+    case 'czerwony':
+        console.log('podales kolor czerwony');
+        break;
+    case 'czarny':
+        console.log('podales kolor czarny');
+        break;
+    default:
+        console.log('jestes daltonista');
+}
+
+//wypisanie wszystkich elementow tablicy za pomoca for
+for (var i = 0; i < tablica.length; i++) {
+    //    console.log(tablica[i]);
+    console.log('Numer elementu: ' + (i + 1) + 'Index: ' + i + ' ' + 'Wartosc: ' + tablica[i]);
+
+    //    if(i == 2) {
+    //        break;
+    //    }
+    /*if(i == 2) {
+        continue;
+    }*/
+}
+
+
+
+//wypisanie elementow tablicy za pomoca forEach (nie trzeba iteratora i=0)
+tablica.forEach(function (element, index) {
+    console.log('Element: ' + element + ' ' + 'o indeksie: ' + index);
+})
+
+//petle while
+var it = 0;
+while (it < 10) {
+    console.log(it);
+    it++;
+}
+
+//do...while
+var iter = 20;
+do {
+    console.log(iter);
+    iter--;
+} while (iter > 10);
+
+for (var b = 0; b < 10; ++b) {
+    if (b == 5) {
+        continue;
+    } else {
+        console.log(b);
+    }
+
+    console.log('--');
+}
+
+//JSON
+var jsonOsoby = {
+        "osoby": [
+            {
+                imie: 'krystian',
+                nazwisko: 'dziopa',
+                zainteresowania: [
+                    {nazwa: 'podroze'},
+                    {nazwa: 'gotowanie'}
+         ]
+        },
+            {
+                imie: 'dominik',
+                nazwisko: 'gaudyn',
+                zainteresowania: [
+                {nazwa: 'akrobatyka'},
+                {nazwa: 'koszykowka'}
+    ] 
+},
+       {  
+                imie: 'anna',
+                nazwisko: 'nowak',
+                zainteresowania: [
+                {nazwa: 'jezyki'},
+                {nazwa: 'ksiazki'}      
+       ]
+}
+                        
+    
+              
+                ]
+        }
+
+/*        for (var i = 0; i < jsonOsoby.osoby.length; i++) {
+            console.log(jsonOsoby.osoby[i].imie + ' ' + jsonOsoby.osoby[i].nazwisko);
+        }
+
+        jsonOsoby.osoby.forEach(function (element, index) {
+            console.log(element.imie + ' ' + element.nazwisko);
+        })
+
+jsonOsoby.osoby.forEach(function (element, index) {
+    console.log(element.imie + ' ' + element.nazwisko + ' ' + element.nazwisko + ' ' + element.zainteresowanie[index].nazwa);
+})*/
+
+for (var i=0; i < jsonOsoby.osoby.length; i++) {
+    for(var j=0; j < jsonOsoby.osoby[i].zainteresowania.length; j++) {
+        console.log(jsonOsoby.osoby[i].zainteresowania[j].nazwa);
+    }
+}
+
+//funkcja z trzema parametrami 
+function iloczyn(param1, param2, param3)
+//zmienna lokalna trzyma iloczyn trzech parametrow
+{
+var wynik = param1 * param2 * param3;
+//    zwracam wynik - zmienna ktora trzyma wynik dzialania
+    return wynik;
+}
+//zmienna globalna do ktorej przypisuje wartosc funkcji iloczyn(podaje parametry)
+var wynikIloczyn = iloczyn(50,2,3);
+//wrzucam do konsoli zmienna, ktora trzyma wynik
+console.log(wynikIloczyn);
+
